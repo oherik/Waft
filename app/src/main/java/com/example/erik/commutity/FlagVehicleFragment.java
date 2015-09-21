9182ec56eed431445d24fa418d35ac992a05ddda
Erik Öhrn <erik.ohrn@outlook.com>
2015-09-21 12:54:49 +0200

    Initial commit for flagging vehicles

---
+++ app/src/main/java/com/example/erik/commutity/FlagVehicleFragment.java
@@ -0,0 +1,22 @@
+package com.example.erik.commutity;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FlagVehicleFragment extends Fragment {
+
+    public FlagVehicleFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_flag_vehicle, container, false);
+    }
+}
