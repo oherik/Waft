11e78e045080404ec3812ec87b9823523a8d12e0
Erik Öhrn <erik.ohrn@outlook.com>
2015-09-22 16:12:45 +0200

    Now goes back to the previous view after the user has clicked on Send

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagVehicleDetailFragment.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagVehicleDetailFragment.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.erik.commutity.R;
 
@@ -26,9 +30,12 @@ public class FlagVehicleDetailFragment extends Fragment {
     final static String ARG_POSITION = "position";
     int mCurrentPosition = -1;
 
+    private final String LOG_TAG = FlagVehicleDetailFragment.class.getSimpleName();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
 
         // If activity recreated (such as from screen rotate), restore
         // the previous article selection set by onSaveInstanceState().
@@ -38,7 +45,29 @@ public class FlagVehicleDetailFragment extends Fragment {
         }
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_flag_vehicle_detail, container, false);
+        View view =  inflater.inflate(R.layout.fragment_flag_vehicle_detail, container, false);
+        Button sendButton = (Button) view.findViewById(R.id.flagDetailSendButton);
+        sendButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(getActivity().getApplicationContext(), "Test ok",
+                        Toast.LENGTH_SHORT).show();
+                //TODO send request here
+
+                //Change back to the previous view
+                FlagVehicleFragment flagFragment = new FlagVehicleFragment();
+                Bundle args = new Bundle();
+                args.putInt(FlagVehicleDetailFragment.ARG_POSITION, mCurrentPosition);
+                flagFragment.setArguments(args);
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.content_frame, flagFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+        return view;
     }
 
     @Override
