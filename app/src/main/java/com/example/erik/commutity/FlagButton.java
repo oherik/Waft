83047991c1f220703782cc1189b9b2d9dc9fe518
Erik Öhrn <erik.ohrn@outlook.com>
2015-09-22 11:28:39 +0200

    Simplified the flag button, it's no longer an ImageButton

--- app/src/main/java/com/example/erik/commutity/FlagButton.java
+++ app/src/main/java/com/example/erik/commutity/FlagButton.java
@@ -1,19 +1,34 @@
 package com.example.erik.commutity;
 
+
+import android.graphics.drawable.Drawable;
+
 /**
  * A class containing data for a flag button
  */
-public class FlagButton {
+public class FlagButton{
+    private Drawable image;
     private String description;
     private FlagType type;
 
     /**
      * Creates a default button
+     * @param image The flag image
      * @param description   The flag description
      * @param type  The type of flag
      */
-    public FlagButton(String description, FlagType type){
+    public FlagButton(Drawable image, String description, FlagType type){
+        this.image = image;
         this.description = description;
         this.type = type;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public Drawable getImage(){
+        return image;
+    }
+    public FlagType getType(){
+        return type;
     }
 }
