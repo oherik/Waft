3798d9967b8722c94433c0048a39f6a39a927335
Erik Öhrn <erik.ohrn@outlook.com>
2015-09-22 13:42:25 +0200

    Moved the flag report data to a specific folder

---
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagButton.java
@@ -0,0 +1,34 @@
+package com.alive_n_clickin.commutity.presentation.flagreport;
+
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * A class containing data for a flag button
+ */
+public class FlagButton{
+    private Drawable image;
+    private String description;
+    private FlagType type;
+
+    /**
+     * Creates a default button
+     * @param image The flag image
+     * @param description   The flag description
+     * @param type  The type of flag
+     */
+    public FlagButton(Drawable image, String description, FlagType type){
+        this.image = image;
+        this.description = description;
+        this.type = type;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public Drawable getImage(){
+        return image;
+    }
+    public FlagType getType(){
+        return type;
+    }
+}
---
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagType.java
@@ -0,0 +1,8 @@
+package com.alive_n_clickin.commutity.presentation.flagreport;
+
+/**
+ * The different flag types available
+ */
+public enum FlagType {
+    FULL, LATE, DIRTY, ROWDY, WHEELCHAIR, STROLLER
+}
