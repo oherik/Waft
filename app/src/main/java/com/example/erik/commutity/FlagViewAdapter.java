6e8d5c5ebe91922cf47531ba25c0b2e59364a8e7
Erik Öhrn <erik.ohrn@outlook.com>
2015-09-22 11:57:34 +0200

    Cleaned up the adapter from unnessecary imports

--- app/src/main/java/com/example/erik/commutity/FlagViewAdapter.java
+++ app/src/main/java/com/example/erik/commutity/FlagViewAdapter.java
@@ -1,18 +1,12 @@
 package com.example.erik.commutity;
 
-import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.List;
 
