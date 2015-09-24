b0e87b141aef38824a53956fd5123aea02af688c
Erik Öhrn <erik.ohrn@outlook.com>
2015-09-24 10:19:11 +0200

    Added values to the flag types

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagType.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagType.java
@@ -4,5 +4,12 @@ package com.alive_n_clickin.commutity.presentation.flagreport;
  * The different flag types available
  */
 public enum FlagType {
-    FULL, LATE, DIRTY, ROWDY, WHEELCHAIR, STROLLER
+
+    OTHER(1),FULL(2), LATE(3), DIRTY(4), ROWDY(5), WHEELCHAIR(6);
+    
+    public final int flagTypeID;
+
+    FlagType(int flagTypeID) {
+        this.flagTypeID = flagTypeID;
+    }
 }
