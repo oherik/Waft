9d97545c26478be3e58423cda38b65bb56a96bca
Oscar Evertsson <oscar.evertsson@live.com>
2015-09-29 11:24:03 +0200

    Created a java class for the main activity.

---
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/MainActivity.java
@@ -0,0 +1,25 @@
+package com.alive_n_clickin.commutity.presentation.flagreport;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.alive_n_clickin.commutity.R;
+
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.main_activity);
+    }
+
+    public void clickHandler(View view){
+        Intent intent = new Intent(this,FlagVehicle.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+}
