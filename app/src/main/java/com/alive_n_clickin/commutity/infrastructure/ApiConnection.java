f37a571055fc76a614870bc108e14e5bfdafa31b
Oscar Evertsson <oscar.evertsson@live.com>
2015-09-30 13:12:38 +0200

    Refactored the ApiConnection and created two new classes for different api connections.

--- app/src/main/java/com/alive_n_clickin/commutity/infrastructure/ApiConnection.java
+++ app/src/main/java/com/alive_n_clickin/commutity/infrastructure/ApiConnection.java
@@ -1,15 +1,8 @@
 package com.alive_n_clickin.commutity.infrastructure;
 
-import android.net.Uri;
-import android.util.Log;
-
-import com.alive_n_clickin.commutity.util.LogUtils;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Scanner;
 
 /**
@@ -21,47 +14,13 @@ import java.util.Scanner;
  */
 class ApiConnection {
 
-    private static final String BASE_URL_ELECTRICITY = "https://ece01.ericsson.net:4443/ecity";
-    //Username and password, base64 encoded
-    private static final String AUTHORIZATION = "Basic Z3JwOToza1B2MmlTU3Nu";
 
     /**
-     * Send a query to electricity. It will be appended to the base url and sent using the credentials of the team
-     * The query must be well formed
-     * @param query the query to be appended. Leave out the '?' at the beginning
-     * @return null if the connection didn't work
-     * @throws MalformedURLException if the query parameter is invalid.
-     */
-    protected String sendGetToElectricity(String query) {
-        Uri.Builder uriBuilder = Uri.parse(BASE_URL_ELECTRICITY).buildUpon();
-        uriBuilder.encodedQuery(query);
-        Uri uri = uriBuilder.build();
-
-        Log.d(LogUtils.getLogTag(this), uri.toString());
-
-        HttpURLConnection connection = null;
-        try {
-            URL url = new URL(uri.toString());
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Authorization", AUTHORIZATION);
-            return getResponseFromHttpConnection(connection);
-        } catch (IOException e) {
-            String errorMessage = "Error connection to Electricity API";
-            if (connection != null) {
-                errorMessage = readStream(connection.getErrorStream());
-            }
-            Log.e(LogUtils.getLogTag(this), errorMessage, e);
-        }
-        return null;
-    }
-
-    /**
-     * Returns the response of a connection. Handles parsing and excpetions
+     * Returns the response of a connection. Handles parsing and exceptions
      * @param connection the HttpURLConnection that the response should be read from
      * @return the response if there is one, null otherwise. Check log messages if null is returned
      */
-    private String getResponseFromHttpConnection(HttpURLConnection connection) {
+    static String getResponseFromHttpConnection(HttpURLConnection connection) {
         try {
             connection.connect();
             InputStream inputStream = connection.getInputStream();
@@ -72,7 +31,6 @@ class ApiConnection {
 
         } catch (IOException e) {
             String errors = readStream(connection.getErrorStream());
-            Log.e(LogUtils.getLogTag(this), errors, e);
         } finally {
             if (connection != null) {
                 connection.disconnect();
@@ -87,7 +45,7 @@ class ApiConnection {
      * @param inputStream
      * @return
      */
-    private String readStream(InputStream inputStream) {
+    static String readStream(InputStream inputStream) {
         Scanner sc = new Scanner(inputStream);
         //By setting delimiter \A (which marks the beginning of the file) the Scanner read the whole file.
         return sc.useDelimiter("\\A").next();
---
+++ app/src/main/java/com/alive_n_clickin/commutity/infrastructure/VasttrafikApiConnection.java
@@ -0,0 +1,56 @@
+package com.alive_n_clickin.commutity.infrastructure;
+
+import android.net.Uri;
+import android.util.Log;
+
+import com.alive_n_clickin.commutity.util.LogUtils;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by OscarEvertsson on 30/09/15.
+ */
+public class VasttrafikApiConnection {
+    private static final String BASE_URL_VASTTRAFIK =
+            "http://api.vasttrafik.se/bin/rest.exe/v1/";
+    private static final String API_KEY = "69b13ace-0bc1-4203-8a56-cc95648f4dca";
+
+    /**
+     * Send a query to Vasttrafik. It will be appended to the base url and sent using the credentials of the team
+     * The query must be well formed.
+     * @param query the query to be appended. Leave out the '?' at the beginning
+     * @return null if the connection didn't work
+     * @throws MalformedURLException if the query parameter is invalid.
+     */
+    public String sendGetToVasttrafik(String path,String query) {
+        Uri.Builder uriBuilder = Uri.parse(BASE_URL_VASTTRAFIK).buildUpon();
+        uriBuilder.appendPath(path);
+        uriBuilder.encodedQuery(query);
+        uriBuilder.appendQueryParameter("authKey", API_KEY);
+        uriBuilder.appendQueryParameter("format","json");
+        uriBuilder.appendQueryParameter("jsonpCallback","processJSON");
+
+        Uri uri = uriBuilder.build();
+
+        Log.d(LogUtils.getLogTag(this), uri.toString());
+
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL(uri.toString());
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            return ApiConnection.getResponseFromHttpConnection(connection);
+        } catch (IOException e) {
+            String errorMessage = "Error connection to Vasttrafik API";
+            if (connection != null) {
+                errorMessage = ApiConnection.readStream(connection.getErrorStream());
+            }
+            Log.e(LogUtils.getLogTag(this), errorMessage, e);
+        }
+        return null;
+
+    }
+}
