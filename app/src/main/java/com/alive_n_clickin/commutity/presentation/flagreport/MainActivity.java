c82577d8b6ca62fc70596e7b558bc12a4434e876
Erik Öhrn <erik.ohrn@outlook.com>
2015-09-30 09:37:53 +0200

    Added a search fragment, although it's currently not working properly

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/MainActivity.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/MainActivity.java
@@ -3,14 +3,20 @@ package com.alive_n_clickin.commutity.presentation.flagreport;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.alive_n_clickin.commutity.R;
+import com.alive_n_clickin.commutity.presentation.search.SearchFragment;
 
 /**
  * This class handles the first view presented to the user.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+    private final String LOG_TAG = FragmentActivity.class.getSimpleName();
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
@@ -25,5 +31,22 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    /**
+     * Switches to another view containing the search window and results
+     * @param view The current view
+     */
+    public void switchToSearchFragment(View view){
+        SearchFragment searchFragment = new SearchFragment();
+        String currentStop = ((TextView) view.findViewById(R.id.currentStop)).getText().toString();
+        Bundle args = new Bundle();
+        args.putString(Intent.EXTRA_TEXT, currentStop);
+        searchFragment.setArguments(args);
+        FragmentTransaction transaction = this.getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_content_frame, searchFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+        Log.e(LOG_TAG, "LOLOL");
     }
 }
---
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/search/SearchFragment.java
@@ -0,0 +1,36 @@
+package com.alive_n_clickin.commutity.presentation.search;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.alive_n_clickin.commutity.R;
+
+/**
+ * A fragment for the search function. Contains a edit text field for inputting a search term
+ * and a list containing the results
+ */
+public class SearchFragment extends Fragment {
+    final static String ARG_POSITION    = "position";
+    int mCurrentPosition                = -1;
+
+    private final String LOG_TAG = getClass().getSimpleName();
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+            }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View rootView   = inflater.inflate(R.layout.search_fragment, container, false);
+        return rootView;
+    }
+}
