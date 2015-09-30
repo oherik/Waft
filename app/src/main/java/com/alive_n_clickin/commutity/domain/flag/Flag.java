898bcd511a894a01914c67d4ecaa953111f9c8bf
Mats Högberg <mats@hgbrg.se>
2015-09-30 23:17:59 +0200

    Moved FlagType enum into Flag class

--- app/src/main/java/com/alive_n_clickin/commutity/domain/flag/Flag.java
+++ app/src/main/java/com/alive_n_clickin/commutity/domain/flag/Flag.java
@@ -6,6 +6,42 @@ import java.util.Date;
  * A concrete implementation of the IFlag interface. Objects of this class are immutable.
  */
 public class Flag implements IFlag {
+    /**
+     * An enum for different flag types. Each flag type is associated with a string and a boolean that
+     * determines whether or not a comment is required for that flag type.
+     */
+    public enum Type implements IFlagType {
+        DELAY ("Försenad", false),
+        CROWDED ("Full", false),
+        MESSY ("Stökig", false),
+        BAD_CLIMATE ("Dåligt klimat", false),
+        VANDALIZED ("Vandalisering", false),
+        OTHER ("Övrigt", true);
+
+        private final String name;
+        private final boolean requiresComment;
+
+        Type(String name, boolean requiresComment) {
+            this.name = name;
+            this.requiresComment = requiresComment;
+        }
+
+        @Override
+        public String getName() {
+            return this.name;
+        }
+
+        @Override
+        public boolean isCommentRequired() {
+            return this.requiresComment;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("FlagType [name=%s]", this.name);
+        }
+    }
+
     private static final int COMMENT_REQUIRED_MINIMUM_LENGTH = 5;
 
     private final IFlagType type;
