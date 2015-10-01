fe67413ff2e5756422c4669c5101cf558a718b0a
Mats Högberg <mats@hgbrg.se>
2015-10-01 17:54:50 +0200

    Removed the IFlaggable interface and the AbstractFlaggable class and moved their functionality into Bus instead. This was to try to keep things as simple as possible from the start

--- app/src/main/java/com/alive_n_clickin/commutity/domain/flaggable/Bus.java
+++ app/src/main/java/com/alive_n_clickin/commutity/domain/flaggable/Bus.java
@@ -1,16 +1,50 @@
 package com.alive_n_clickin.commutity.domain.flaggable;
 
+import com.alive_n_clickin.commutity.domain.flag.IFlag;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A concrete implementation of the IBus interface.
  */
-public class Bus extends AbstractFlaggable implements IBus {
+public class Bus implements IBus {
     private String DGW;
     private String VIN;
     private String licenseNumber;
     private String wifiBSSID;
 
+    private List<IFlag> flags = new ArrayList<>();
+
     /**
-     * Instantiates a new bus with the supplied DGW, VIN, licenseNumber and wifiBSSID.
+     * Instantiates a new bus with the supplied DGW, VIN, licenseNumber, wifiBSSID and flags.
+     *
+     * @param DGW The Device Gateway for the bus. For example "Ericsson$100020".
+     * @param VIN The Vehicle Identification Number for the bus. For example "YV3U0V222FA100020".
+     * @param licenseNumber The number on the license plate of the bus. For example "EPO 131".
+     * @param wifiBSSID The BSSID for the WiFi connection on board the bus. For example "00:13:95:13:49:f5".
+     * @param flags The flags that this bus should be flagged with. Can be null.
+     * @throws IllegalArgumentException if any of the parameters are null.
+     */
+    public Bus(String DGW, String VIN, String licenseNumber, String wifiBSSID, List<IFlag> flags) {
+        // TODO: Add stricter checking of the parameters to ensure that all the ID:s are valid?
+        if (DGW == null
+                || VIN == null
+                || licenseNumber == null
+                || wifiBSSID == null
+                || flags == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.DGW = DGW;
+        this.VIN = VIN;
+        this.licenseNumber = licenseNumber;
+        this.wifiBSSID = wifiBSSID;
+        this.flags = new ArrayList<>(flags);
+    }
+
+    /**
+     * Instantiates a new bus with the supplied DGW, VIN, licenseNumber, wifiBSSID.
      *
      * @param DGW The Device Gateway for the bus. For example "Ericsson$100020".
      * @param VIN The Vehicle Identification Number for the bus. For example "YV3U0V222FA100020".
@@ -19,18 +53,7 @@ public class Bus extends AbstractFlaggable implements IBus {
      * @throws IllegalArgumentException if any of the parameters are null.
      */
     public Bus(String DGW, String VIN, String licenseNumber, String wifiBSSID) {
-        // TODO: Add stricter checking of the parameters to ensure that all the ID:s are valid?
-        if (DGW == null
-                || VIN == null
-                || licenseNumber == null
-                || wifiBSSID == null) {
-            throw new IllegalArgumentException();
-        }
-
-        this.DGW = DGW;
-        this.VIN = VIN;
-        this.licenseNumber = licenseNumber;
-        this.wifiBSSID = wifiBSSID;
+        this(DGW, VIN, licenseNumber, wifiBSSID, new ArrayList<IFlag>());
     }
 
     @Override
@@ -53,10 +76,16 @@ public class Bus extends AbstractFlaggable implements IBus {
         return wifiBSSID;
     }
 
+    @Override
+    public List<IFlag> getFlags() {
+        return new ArrayList<>(this.flags);
+    }
+
     /**
      * {@inheritDoc}<br><br>
      *
-     * Two buses are equal if their DGW, VIN, licenseNumber and wifiBSSID are equal.
+     * Two buses are equal if their DGW, VIN, licenseNumber and wifiBSSID are equal, and if all
+     * their flags are equal.
      */
     @Override
     public boolean equals(Object obj) {
@@ -71,7 +100,8 @@ public class Bus extends AbstractFlaggable implements IBus {
         return this.DGW.equals(other.DGW)
                 && this.VIN.equals(other.VIN)
                 && this.licenseNumber.equals(other.licenseNumber)
-                && this.wifiBSSID.equals(other.wifiBSSID);
+                && this.wifiBSSID.equals(other.wifiBSSID)
+                && this.flags.equals(other.flags);
     }
 
     @Override
--- app/src/main/java/com/alive_n_clickin/commutity/domain/flaggable/IBus.java
+++ app/src/main/java/com/alive_n_clickin/commutity/domain/flaggable/IBus.java
@@ -1,10 +1,14 @@
 package com.alive_n_clickin.commutity.domain.flaggable;
 
+import com.alive_n_clickin.commutity.domain.flag.IFlag;
+
+import java.util.List;
+
 /**
- * An interface for modelling a bus. This interface extends the IFlaggable interface, since
- * all buses should be flaggable.
+ * An interface for modelling a bus. A bus should be immutable. All methods that returns
+ * mutable objects should return copies of said object, to ensure immutability.
  */
-public interface IBus extends IFlaggable {
+public interface IBus {
     /**
      * @return the Device Gateway id of this bus. This ID is used by ElectriCity to identify the bus.
      */
@@ -25,4 +29,10 @@ public interface IBus extends IFlaggable {
      * @return the BSSID for the WiFi connection on board the bus.
      */
     String getWifiBSSID();
+
+    /**
+     * @return a list of all flags on this bus. This method should never return null. If the bus
+     * doesn't have anyg flags, it should return an empty list.
+     */
+    List<IFlag> getFlags();
 }
