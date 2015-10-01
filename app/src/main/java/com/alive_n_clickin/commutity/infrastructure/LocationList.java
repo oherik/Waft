4dc8a415ac854507e1665648a6a7f03227302ede
Oscar Evertsson <oscar.evertsson@live.com>
2015-10-01 15:13:23 +0200

    Created a class to represent the data fetched from the vasttrafik api.

---
+++ app/src/main/java/com/alive_n_clickin/commutity/infrastructure/LocationList.java
@@ -0,0 +1,27 @@
+package com.alive_n_clickin.commutity.infrastructure;
+
+import java.util.List;
+
+/**
+ * Created by OscarEvertsson on 30/09/15.
+ */
+public class LocationList {
+    private String noNamespaceSchemaLocation;
+    private String serverTime;
+    private String serverDate;
+    private List<Stop> StopLocation;
+
+    public List<Stop> getStopLocations(){
+        return this.StopLocation;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationList{" +
+                "noNamespaceSchemaLocation='" + noNamespaceSchemaLocation + '\'' +
+                ", serverTime='" + serverTime + '\'' +
+                ", serverDate='" + serverDate + '\'' +
+                ", StopLocation=" + StopLocation +
+                '}';
+    }
+}
