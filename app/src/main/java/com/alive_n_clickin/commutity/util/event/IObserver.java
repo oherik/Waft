4b15a183fd4ae9235759c62b50f0c89224258b50
Mats Högberg <mats@hgbrg.se>
2015-10-02 14:53:35 +0200

    Added missing documentation to the IObserver interface

--- app/src/main/java/com/alive_n_clickin/commutity/util/event/IObserver.java
+++ app/src/main/java/com/alive_n_clickin/commutity/util/event/IObserver.java
@@ -8,5 +8,11 @@ package com.alive_n_clickin.commutity.util.event;
  * @param <E> the type of event this observer wants to receive.
  */
 public interface IObserver<E extends IEvent> {
+    /**
+     * This method gets called when an event gets sent from an observable that this observer
+     * listens to. Each implementation decides what to do when this method gets called.
+     *
+     * @param event the event that occurred.
+     */
     void onEvent(E event);
 }
