a6766955352faa925cc3957f1c87e1afd5fa44e3
Erik Öhrn <erik.ohrn@outlook.com>
2015-10-02 09:53:15 +0200

    Commented and cleaned up classes. The current stop is now stored in the main activity for easy access

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/main/MainFragment.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/main/MainFragment.java
@@ -4,35 +4,27 @@ import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
-import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import lombok.Getter;
+
 import com.alive_n_clickin.commutity.R;
-import com.alive_n_clickin.commutity.infrastructure.WifiBroadcastReceiver;
-import com.alive_n_clickin.commutity.infrastructure.WifiHelper;
 import com.alive_n_clickin.commutity.infrastructure.api.Stop;
-import com.alive_n_clickin.commutity.presentation.flagreport.FlagVehicle;
-import com.alive_n_clickin.commutity.presentation.search.SearchFragment;
 
 /**
- * This class handles the first view presented to the user.
+ * This class handles the first view presented to the user. It holds the stop the user has selected,
+ * as well as handling the switching to the search fragment when the user presses the stop text view.
  */
-//TODO Make the UI a fragment to ease switching
+
 public class MainFragment extends Fragment {
-    private final String LOG_TAG = FragmentActivity.class.getSimpleName();
-    private Stop currentStop;
+
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,12 +32,11 @@ public class MainFragment extends Fragment {
         final View rootView = inflater.inflate(R.layout.main_fragment, container, false);
         TextView stopTextView = (TextView) rootView.findViewById(R.id.currentStop);
 
-        Bundle args = getArguments();
-
-        //If a stop is received, set it as the current stop
-        if(args!=null) {
-            currentStop = (Stop) args.getSerializable(Intent.EXTRA_RETURN_RESULT);
-            stopTextView.setText(currentStop.getName());
+        //Set stop name
+        MainActivity mainActivity = (MainActivity) getActivity();
+        if(mainActivity.getCurrentStop()!=null && !("".equals(mainActivity.getCurrentStop().getName())))
+        {
+            stopTextView.setText(mainActivity.getCurrentStop().getName());
         }
 
         //Underline the text
