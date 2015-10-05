856a553b8ffa4c46a92ea11e935f9e0c536aebd9
Erik Öhrn <erik.ohrn@outlook.com>
2015-10-05 17:05:12 +0200

    Added list view

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/main/MainFragment.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/main/MainFragment.java
@@ -3,12 +3,21 @@ package com.alive_n_clickin.commutity.presentation.main;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.alive_n_clickin.commutity.R;
+import com.alive_n_clickin.commutity.infrastructure.api.Stop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.NonNull;
 
 /**
  * This class handles the first view presented to the user. It holds the stop the user has selected,
@@ -17,6 +26,12 @@ import com.alive_n_clickin.commutity.R;
  */
 
 public class MainFragment extends Fragment {
+    private int maxNumberOfBusesInList = 10;
+    private TextView stopTextView;
+    private ListView busListView;
+    private ArrayAdapter<String> adapter;
+
+    private final String LOG_TAG = getClass().getSimpleName();
 
     @Override
     public void onCreate(Bundle bundle) {
@@ -27,23 +42,84 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.main_fragment, container, false);
+        stopTextView = (TextView) rootView.findViewById(R.id.currentStop);
+        busListView = (ListView) rootView.findViewById(R.id.busListView);
+        MainActivity mainActivity = (MainActivity) getActivity();
+        Stop currentStop = mainActivity.getCurrentStop();
+        //TODO add custom adapter
+        List<String> adapterData = new ArrayList<>();
+        adapter = new ArrayAdapter<>(getActivity().getBaseContext(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, adapterData);
 
-        //Set stop name
-       setStopName(rootView);
+        setStopName(currentStop);
+        populateBusList(currentStop, rootView);
 
         return rootView;
     }
 
     /**
-     * Sets the stop name, as well as formatting the text
-     * @param currentView The view in which the text view resides
+     * Sets the buses in the bus list view, given the current stop
+     * @param currentStop The stop the displayed buses are headed to
+     * @param view The view that's currently focused
      */
-    private void setStopName(View currentView){
-        TextView stopTextView = (TextView) currentView.findViewById(R.id.currentStop);
-        MainActivity mainActivity = (MainActivity) getActivity();
-        if(mainActivity.getCurrentStop()!=null && !("".equals(mainActivity.getCurrentStop().getName())))
-        {
-            stopTextView.setText(mainActivity.getCurrentStop().getName());
+    private void populateBusList(Stop currentStop, @NonNull View view){
+        if(currentStop==null){
+            busListView.setVisibility(view.INVISIBLE);
+        } else {
+            List<String> buses = getBuses(currentStop, maxNumberOfBusesInList);
+            if(buses!=null && !buses.isEmpty()) {
+                busListView.setVisibility(view.VISIBLE);
+                adapter.clear();
+                adapter.addAll(buses);
+            }
+        }
+    }
+
+
+        //TODO return type should be ArrayList<Bus>, but that class doesn't exist yet
+    /**
+     * Returns the different buses arriving to this stop
+     * @param currentStop This bus stop
+     * @param numberOfBuses The number of buses to return
+     * @throw IllegalArgumentException if the number of buses is less
+     * than one
+     * @return The buses on the way to the desired stop, null if no buses are found
+     */
+    private List<String> getBuses(@NonNull Stop currentStop, int numberOfBuses) throws IllegalArgumentException{
+        if(numberOfBuses<1){
+            throw new IllegalArgumentException("Number of buses must be greater than or equal to " +
+                    "one");
+        }
+        //TODO get data from bus manager
+        List<String> testBuses = new ArrayList<>();
+        testBuses.add("Ettan");
+        testBuses.add("Tvåan");
+        testBuses.add("Trean");
+        testBuses.add("Fyran");
+        testBuses.add("Femman");
+        testBuses.add("Sexan");
+        testBuses.add("Sjuan");
+        testBuses.add("Nian");
+        testBuses.add("Tian");
+        testBuses.add("Elvan");
+        testBuses.add("Tolvan");
+
+        if(!testBuses.isEmpty()) {
+            int maxIndex = Math.min(numberOfBuses, testBuses.size()) - 1;
+            return testBuses.subList(0, maxIndex);
+        }
+        Log.d(LOG_TAG, testBuses.get(0));
+        return testBuses;
+
+     }
+
+    /**
+     * Sets the stop name, as well as formatting the text
+     * @param currentStop The stop to be displayed
+     */
+    private void setStopName(Stop currentStop){
+        if(currentStop!=null && !("".equals(currentStop.getName()))) {
+            stopTextView.setText(currentStop.getName());
         }
         stopTextView.setPaintFlags(stopTextView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
