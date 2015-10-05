bf5d81f0ea8d835923ffd1ab62582322f246caca
Mats Högberg <mats@hgbrg.se>
2015-10-05 09:09:30 +0200

    Added a new class that extends Application and updated the manifest file

---
+++ app/src/main/java/com/alive_n_clickin/commutity/MyApplication.java
@@ -0,0 +1,35 @@
+package com.alive_n_clickin.commutity;
+
+import android.app.Application;
+
+import com.alive_n_clickin.commutity.application.BusManager;
+import com.alive_n_clickin.commutity.application.IBusManager;
+import com.alive_n_clickin.commutity.application.NearbyBusScanner;
+import com.alive_n_clickin.commutity.infrastructure.WifiBroadcastReceiver;
+
+/**
+ * This is the "main" class for the application. This class is responsible for keeping track of
+ * everything that should be accessible application wide, and for creating instances of all
+ * broadcast receivers and services that always should be active and can't be declared in the
+ * manifest for whatever reason.
+ */
+public class MyApplication extends Application {
+    private IBusManager busManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        WifiBroadcastReceiver wifiBroadcastReceiver = new WifiBroadcastReceiver(this);
+        NearbyBusScanner nearbyBusScanner = new NearbyBusScanner(wifiBroadcastReceiver);
+
+        this.busManager = new BusManager(nearbyBusScanner);
+    }
+
+    /**
+     * @return the BusManager instance that is used throughout the application.
+     */
+    public IBusManager getBusManager() {
+        return this.busManager;
+    }
+}
