8e011cae1973977f7317085d05606f887b09644d
Oscar Evertsson <oscar.evertsson@live.com>
2015-10-06 10:41:41 +0200

    Updated with a getTimeToArrival

--- app/src/main/java/com/alive_n_clickin/commutity/infrastructure/api/ArrivingVehicle.java
+++ app/src/main/java/com/alive_n_clickin/commutity/infrastructure/api/ArrivingVehicle.java
@@ -2,6 +2,7 @@ package com.alive_n_clickin.commutity.infrastructure.api;
 
 import com.alive_n_clickin.commutity.domain.Bus;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import lombok.Getter;
@@ -10,17 +11,28 @@ import lombok.NonNull;
 /** Consists of a vehicle and when it will arrive to a certain stop
  */
 public class ArrivingVehicle {
+    private Date arrivalTime;
     @Getter private Bus bus;
-    @Getter private Date arrival;
+
 
     /**
      * Constructor
      * @param bus
-     * @param arrival
+     * @param arrivalTime
      */
-    public ArrivingVehicle(@NonNull Bus bus, @NonNull Date arrival){
+    public ArrivingVehicle(@NonNull Bus bus, @NonNull Date arrivalTime){
         this.bus = bus;
-        this.arrival = arrival;
+        this.arrivalTime = arrivalTime;
+    }
+
+    /**
+     * This method provides the differences between the arrival time and current time.
+     * @return
+     */
+    public Calendar getTimeToArrival() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date(this.arrivalTime.getTime() - new Date().getTime()));
+        return calendar;
     }
 
 }
