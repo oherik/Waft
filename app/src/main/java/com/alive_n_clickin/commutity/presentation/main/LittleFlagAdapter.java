7c0c8d53819bc18cc3cca8a2f2d76a2bc50f0cdb
Oscar Evertsson <oscar.evertsson@live.com>
2015-10-06 19:11:30 +0200

    Updated documentation and updated code to fit the style guide.

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/main/LittleFlagAdapter.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/main/LittleFlagAdapter.java
@@ -14,7 +14,7 @@ import com.alive_n_clickin.commutity.domain.IFlag;
 import java.util.List;
 
 /**
- *
+ * This class only purposes is to help with the insertion of each flag within the {@link ListAdapter}'s GridView.
  */
 public class LittleFlagAdapter extends ArrayAdapter<IFlag> {
 
@@ -22,16 +22,17 @@ public class LittleFlagAdapter extends ArrayAdapter<IFlag> {
         super(context,0,flagList);
     }
 
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        IFlag flag = getItem(position);
+        IFlag flag = getItem(position); //TODO: Use this flag object instead of hardcoding to get the image.
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.little_flag, parent, false);
         }
 
-        ImageView flagImageView = (ImageView) convertView.findViewById(R.id.littleFlagImageView);;
+        ImageView flagImageView = (ImageView) convertView.findViewById(R.id.littleFlagImageView);
         Drawable d = getContext().getResources().getDrawable(R.drawable.flag_delayed_300px);
         flagImageView.setImageDrawable(d);
-        //TODO: Get the image and set it
+        //TODO: Get the image belonging to this flag and set it (currently same image for each flag)
 
         return convertView;
     }
