a1982858524482b834601ce4de2d1886de8cfc5c
Mats Högberg <mats@hgbrg.se>
2015-10-07 19:10:20 +0200

    Added documentation to WifiStateChangeEvent

--- app/src/main/java/com/alive_n_clickin/commutity/event/WifiStateChangeEvent.java
+++ app/src/main/java/com/alive_n_clickin/commutity/event/WifiStateChangeEvent.java
@@ -2,17 +2,25 @@ package com.alive_n_clickin.commutity.event;
 
 import com.alive_n_clickin.commutity.util.event.IEvent;
 
-// TODO: Documentation
+/**
+ * An event for when the state of the wifi changes from disabled to enabled, or vice versa.
+ * The event contains information about the current state of the wifi.
+ */
 public class WifiStateChangeEvent implements IEvent {
-
     private boolean wifiEnabled;
 
-    // TODO: Documentation
+    /**
+     * Initiates a new WifiStateChangeEvent.
+     *
+     * @param wifiEnabled the current state of the wifi. True if enabled, false if disabled.
+     */
     public WifiStateChangeEvent(boolean wifiEnabled) {
         this.wifiEnabled = wifiEnabled;
     }
 
-    // TODO: Documentation
+    /**
+     * @return true if wifi is enabled, false if disabled.
+     */
     public boolean isWifiEnabled() {
         return this.wifiEnabled;
     }
