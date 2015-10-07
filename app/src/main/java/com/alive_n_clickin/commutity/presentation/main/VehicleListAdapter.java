224596467142368c7567cf1f02eef28d10b9a825
hjorthjort <rikard.hjort@gmail.com>
2015-10-07 22:00:14 +0200

    Remove unused variables

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/main/VehicleListAdapter.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/main/VehicleListAdapter.java
@@ -1,12 +1,10 @@
 package com.alive_n_clickin.commutity.presentation.main;
 
 import android.content.Context;
-import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.GridLayout;
 import android.widget.TextView;
@@ -52,13 +50,11 @@ public class VehicleListAdapter extends ArrayAdapter<ApiArrival> {
         TextView timeUntilArrival = (TextView) convertView.findViewById(R.id.timeUntilArrival);
         timeUntilArrival.setText(realDiffInMinutes + "");
 
+
         GridLayout flagListView = (GridLayout) convertView.findViewById(R.id.flagListView);
         flagListView.removeAllViews();
         // Use the adapter for setting all the flags to the list item.
 
-        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
-        Point displaySize = new Point();
-        wm.getDefaultDisplay().getSize(displaySize);
         int maxWidth = parent.getMeasuredWidth();
         int currentWidth = 0;
         for (IFlag flag : vehicle.getFlags()) {
