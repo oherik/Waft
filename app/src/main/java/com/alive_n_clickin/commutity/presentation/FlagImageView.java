8ff9589c5596c8e20feecfb8d32bb119484a7792
hjorthjort <rikard.hjort@gmail.com>
2015-10-07 17:13:23 +0200

    Implements real version of setFlags

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/FlagImageView.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/FlagImageView.java
@@ -6,7 +6,11 @@ import android.util.AttributeSet;
 import android.widget.ImageView;
 
 import com.alive_n_clickin.commutity.R;
+import com.alive_n_clickin.commutity.domain.Flag;
 import com.alive_n_clickin.commutity.domain.IFlag;
+import com.alive_n_clickin.commutity.domain.IFlagType;
+
+import java.util.HashMap;
 
 /**
  * @author hjorthjort
@@ -15,14 +19,23 @@ import com.alive_n_clickin.commutity.domain.IFlag;
 public class FlagImageView extends ImageView {
 
     private IFlag flag = null;
+    private HashMap<IFlagType, Integer> images;
 
     public FlagImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        images = new HashMap<>();
+        images.put(Flag.Type.DELAYED, R.drawable.flag_delayed_300px);
+        images.put(Flag.Type.DISTURBANCES, R.drawable.flag_rowdy_300px);
+        images.put(Flag.Type.MESSY, R.drawable.flag_dirty_alt1_300px);
+        images.put(Flag.Type.OVERCROWDED, R.drawable.flag_full_300px);
+        images.put(Flag.Type.NO_PRAMS, R.drawable.flag_pram_300px);
+        images.put(Flag.Type.OTHER, R.drawable.flag_other_black_300px);
     }
 
     public void setFlag(IFlag flag) {
         this.flag = flag;
-        Drawable d = getResources().getDrawable(R.drawable.flag_delayed_300px);
+        int flagImageResourceNumber = images.get(flag.getType());
+        Drawable d = getResources().getDrawable(flagImageResourceNumber);
         setImageDrawable(d);
     }
 }
