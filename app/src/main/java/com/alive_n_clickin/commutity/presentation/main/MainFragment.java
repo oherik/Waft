550e572aa4c4036130fdbd4cf5b600112c2c54ba
Erik Öhrn <erik.ohrn@outlook.com>
2015-10-07 09:25:32 +0200

    Cleaned up and commented MainFragment

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/main/MainFragment.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/main/MainFragment.java
@@ -36,8 +36,6 @@ public class MainFragment extends Fragment {
     private ListAdapter adapter;
     private List<ArrivingVehicle> arrivingVehicles;
 
-    private final String LOG_TAG = getClass().getSimpleName();
-
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
@@ -53,10 +51,6 @@ public class MainFragment extends Fragment {
         MainActivity mainActivity = (MainActivity) getActivity();
         Stop currentStop = mainActivity.getCurrentStop();
 
-
-        //TODO:Fetch real ArrivingVehicle list
-        List<ArrivingVehicle> adapterData = new ArrayList<>();
-
         busListView = (ListView) rootView.findViewById(R.id.busListView);
         adapter = new ListAdapter(getActivity(),arrivingVehicles);
         busListView.setAdapter(adapter);
@@ -68,7 +62,8 @@ public class MainFragment extends Fragment {
     }
 
     /**
-     * Sets the buses in the bus list view, given the current stop
+     * Sets the list view to either visible or invisible, and then starts the class for adding
+     * vehicles
      * @param currentStop The stop the displayed buses are headed to
      * @param view The view that's currently focused
      */
@@ -76,76 +71,24 @@ public class MainFragment extends Fragment {
         if(currentStop==null){
             busListView.setVisibility(view.INVISIBLE);
         } else {
-
-            //List<String> buses = getBuses(currentStop, maxNumberOfBusesInList);
-            GetBusesFromAPI getBuses = new GetBusesFromAPI();
-            getBuses.execute(currentStop);
-            /*
-            if(buses!=null && !buses.isEmpty()) {
-
-            List<ArrivingVehicle> arrivingVehicles = getBuses(currentStop, maxNumberOfBusesInList);
-            if(arrivingVehicles!=null && !arrivingVehicles.isEmpty()) {
->>>>>>> 0d574eb12a9d751a259312ab4187dfadfa5842b0
-                busListView.setVisibility(view.VISIBLE);
-                adapter.clear();
-                adapter.addAll(arrivingVehicles);
-            }
-            */
+            busListView.setVisibility(view.VISIBLE);
+            AddVehiclesFromAPI addVehicles = new AddVehiclesFromAPI();
+            addVehicles.execute(currentStop);
         }
     }
 
-
-        //TODO return type should be ArrayList<Bus>, but that class doesn't exist yet
     /**
-     * Returns the different buses arriving to this stop
-     * @param currentStop This bus stop
-     * @param numberOfBuses The number of buses to return
-     * @throw IllegalArgumentException if the number of buses is less
-     * than one
-     * @return The buses on the way to the desired stop, null if no buses are found
+     * Collects the vehicles from the adapter, based on which stop is currently selected.
+     * After the class has fetched the results it adds the new data to the list adapter.
      */
-    private List<ArrivingVehicle> getBuses(@NonNull Stop currentStop, int numberOfBuses)
-            throws IllegalArgumentException{
-        if(numberOfBuses<1){
-            throw new IllegalArgumentException("Number of buses must be greater than or equal to " +
-                    "one");
-        }
-
-        GetBusesFromAPI test = new GetBusesFromAPI();
-        test.execute(currentStop);
-
-        //TODO get data from bus manager
-        List<ArrivingVehicle> testBuses = new ArrayList<>();
-        testBuses.add(new ArrivingVehicle());
-        /*testBuses.add("Tvåan");
-        testBuses.add("Trean");
-        testBuses.add("Fyran");
-        testBuses.add("Femman");
-        if(currentStop.getName().equals("GÖTEBORG")) {  //TODO test to see if update works
-            testBuses.add("Sexan");
-            testBuses.add("Sjuan");
-            testBuses.add("Nian");
-            testBuses.add("Tian");
-            testBuses.add("Elvan");
-            testBuses.add("Tolvan");
-        }
-
-        if(!testBuses.isEmpty()) {
-            int maxIndex = Math.min(numberOfBuses, testBuses.size()) - 1;
-            return testBuses.subList(0, maxIndex);
-        }*/
-        return testBuses;
-
-
-     }
-
-    private class GetBusesFromAPI extends AsyncTask<Stop, Void, List<ArrivingVehicle>>{
+    private class AddVehiclesFromAPI extends AsyncTask<Stop, Void, List<ArrivingVehicle>>{
 
         @Override
         protected List<ArrivingVehicle> doInBackground(Stop... params) {
             IVasttrafikAdapter vasttrafikAdapter = ApiAdapterFactory.createVasttrafikAdapter();
             return vasttrafikAdapter.getVehiclesHeadedToStop(params[0]);
         }
+
         @Override
         protected void onPostExecute(List<ArrivingVehicle> result) {
             try {
@@ -158,8 +101,6 @@ public class MainFragment extends Fragment {
                 arrivingVehicles = new ArrayList<>();
             }
             if (result != null) {
-                //The list arrives unsorted. Fix that.
-                //Collections.sort(result);
                 /*
                 While making the API call it's not possible to select how many vehicles the list
                 should contain. This has to be done manually.
@@ -167,8 +108,6 @@ public class MainFragment extends Fragment {
                 result = trimmedList(result, maxNumberOfBusesInList);
                 adapter.clear();
                 adapter.addAll(result);
-                //arrivingVehicles.addAll(result);
-                //adapter.notifyDataSetChanged();
             }
         }
     }
