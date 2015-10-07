9760ca7ab0b3084670ccb10a57bcef23949e790a
Mats Högberg <mats@hgbrg.se>
2015-10-07 19:02:01 +0200

    Added documentation to CurrentBusChangeEvent

--- app/src/main/java/com/alive_n_clickin/commutity/event/CurrentBusChangeEvent.java
+++ app/src/main/java/com/alive_n_clickin/commutity/event/CurrentBusChangeEvent.java
@@ -4,15 +4,24 @@ import com.alive_n_clickin.commutity.domain.IBus;
 import com.alive_n_clickin.commutity.util.event.IEvent;
 
 /**
- * Created by mats on 02/10/15.
+ * An event for when the bus the user is on changes. The event contains information about which
+ * bus the user is currently on.
  */
 public class CurrentBusChangeEvent implements IEvent {
     private IBus bus;
 
+    /**
+     * Initiates a new CurrentBusChangeEvent.
+     *
+     * @param bus the bus that the user is currently on. Can be null.
+     */
     public CurrentBusChangeEvent(IBus bus) {
         this.bus = bus;
     }
 
+    /**
+     * @return the bus the user is currently on. Null if the user isn't on a bus.
+     */
     public IBus getBus() {
         return this.bus;
     }
