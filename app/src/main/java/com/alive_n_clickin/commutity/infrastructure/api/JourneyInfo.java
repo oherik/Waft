011959fecfdb7d3ccb2c1204dc947de7a8759b4d
hjorthjort <rikard.hjort@gmail.com>
2015-10-08 18:46:04 +0200

    Fix encapsulation of Journey info

--- app/src/main/java/com/alive_n_clickin/commutity/infrastructure/api/JourneyInfo.java
+++ app/src/main/java/com/alive_n_clickin/commutity/infrastructure/api/JourneyInfo.java
@@ -2,13 +2,15 @@ package com.alive_n_clickin.commutity.infrastructure.api;
 
 import java.io.Serializable;
 
+import lombok.Getter;
+
 /**
  * @author hjorthjort
  *         Created 08/10/15
  */
 public class JourneyInfo implements Serializable {
-    String resourceSpec;
-    long timestamp;
-    String value;
-    String gatewayId;
+    @Getter private String resourceSpec;
+    @Getter private long timestamp;
+    @Getter private String value;
+    @Getter private String gateway;
 }
