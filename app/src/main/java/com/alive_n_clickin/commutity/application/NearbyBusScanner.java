107ea866f645e09ec8f380eb352feaa0c22b0558
hjorthjort <rikard.hjort@gmail.com>
2015-10-08 15:27:16 +0200

    Get journey info response from EC

--- app/src/main/java/com/alive_n_clickin/commutity/application/NearbyBusScanner.java
+++ app/src/main/java/com/alive_n_clickin/commutity/application/NearbyBusScanner.java
@@ -45,6 +45,7 @@ public class NearbyBusScanner implements IObserver, IObservable {
         buses.put("d0:c7:89:33:27:3e", "M-salarna");
         buses.put("28:c6:8e:71:8a:b8", "Casa del Hjort");
         buses.put("44:ad:d9:f1:51:6e", "Hubben studierum");
+        buses.put("24:de:c6:3b:21:c4", "Launchpad");
         buses.put("d0:c7:89:33:27:3e", "EDUROAM");
         buses.put("88:1d:fc:41:92:90", "EDUROAM");
     }
