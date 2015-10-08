d70eeeefcbf76d275c2f56122d44684c8686eafd
Erik Öhrn <erik.ohrn@outlook.com>
2015-10-08 14:05:58 +0200

    Added missing documentation

--- app/src/main/java/com/alive_n_clickin/commutity/domain/ArrivingVehicle.java
+++ app/src/main/java/com/alive_n_clickin/commutity/domain/ArrivingVehicle.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 import lombok.NonNull;
 
 /**
- * A class containing the same variables as a vehicle, in addition to the stop it's arriving to
- * and when it will get there. It also contains the flags for that vehicle.
+ * A class containing the same variables as a vehicle, in addition to a variable showing when it
+ * will arrive to a specified stop. It also contains the flags for that vehicle.
  */
 public class ArrivingVehicle extends Vehicle {
     @Getter private long timeToArrival;
@@ -16,16 +16,20 @@ public class ArrivingVehicle extends Vehicle {
 
     /**
      * Constructor.
-     * @param destination
-     * @param lineNumber
-     * @param journeyID from Vasttrafik
-     * @param timeToArrival in milliseconds
+     * @param destination Where the vehicle is headed, e.g. "Sahlgrenska"
+     * @param shortRouteName    The short version of the route name, usually simply a line number
+     *                          (e.g. "55"), but can also be longer, e.g. "Grön Express"
+     * @param journeyID from Vasttrafik. This is the unique identification number for a certain
+     *                  route. It gets changed any time the vehicle arrives to the end stop and
+     *                  continues in the opposite direction.
+     * @param timeToArrival in milliseconds. This should be the real time to arrival, not the
+     *                      scheduled
      * @param flags can be null if no flags exist for this vehicle
      * @throws NullPointerException if any parameter is null
      */
-    public ArrivingVehicle(@NonNull String destination, @NonNull String lineNumber, long journeyID, long timeToArrival,
-                           @NonNull List<IFlag> flags){
-        super(destination, lineNumber, journeyID);
+    public ArrivingVehicle(@NonNull String destination, @NonNull String shortRouteName,
+                           long journeyID, long timeToArrival, @NonNull List<IFlag> flags){
+        super(destination, shortRouteName, journeyID);
         this.timeToArrival = timeToArrival;
         this.flags = new ArrayList<>(flags);
     }
