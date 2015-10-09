8a0af8d3b87fc541753f9864c0c52d48cb45abc6
Oscar Evertsson <oscar.evertsson@live.com>
2015-10-09 14:48:22 +0200

    Updated return value.

--- app/src/main/java/com/alive_n_clickin/commutity/infrastructure/api/IWaftAdapter.java
+++ app/src/main/java/com/alive_n_clickin/commutity/infrastructure/api/IWaftAdapter.java
@@ -2,6 +2,7 @@ package com.alive_n_clickin.commutity.infrastructure.api;
 
 import com.alive_n_clickin.commutity.domain.IBus;
 import com.alive_n_clickin.commutity.domain.IFlag;
+import com.alive_n_clickin.commutity.domain.JsonFlag;
 
 import java.util.List;
 
@@ -12,7 +13,7 @@ public interface IWaftAdapter {
      * @param journeyId the id to get flags for.
      * @return a list of IFlags. If there are no flags the list is empty.
      */
-    List<IFlag> getFlagsForVehicle(int journeyId);
+    List<JsonFlag> getFlagsForVehicle(int journeyId);
 
     /**
      * Flags the specified bus with the given flag.
