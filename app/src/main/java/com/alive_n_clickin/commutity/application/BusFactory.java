ec760ef6498f650828cd712c1b2ce96f7554bf55
hjorthjort <rikard.hjort@gmail.com>
2015-10-09 10:18:45 +0200

    Use real dgw

--- app/src/main/java/com/alive_n_clickin/commutity/application/BusFactory.java
+++ app/src/main/java/com/alive_n_clickin/commutity/application/BusFactory.java
@@ -17,24 +17,22 @@ import java.util.ArrayList;
 public class BusFactory {
 
     /**
-     * Takes a DGW and returns a new bus object with all the data for the bus with that DGW.
+     * Takes a dgw and returns a new bus object with all the data for the bus with that DGW.
      *
-     * @param DGW the DGW id for the bus you want to have.
+     * This method may not be called from the UI thread.
+     *
+     * @param dgw the dgw id for the bus you want to have.
      * @return a new bus object.
      */
-    public static IBus getBus(String DGW) {
-        // TODO: Build bus from real data
-        // 1. get information from ElectriCity's API and our backend
-        // 2. create and return bus object
-
+    public static IBus getBus(String dgw) {
         IElectricityAdapter ecAdapter = ApiAdapterFactory.createElectricityAdapter();
-        Journey journey = ecAdapter.getJourneyInfo("Ericsson$100021");
+        Journey journey = ecAdapter.getJourneyInfo(dgw);
         String destination = "";
         String journeyId = "";
         if (journey != null) {
             destination = journey.getDestination();
             journeyId = journey.getJourneyId();
         }
-        return new Bus(DGW, destination, journeyId, "routeNumber", new ArrayList<IFlag>());
+        return new Bus(dgw, destination, journeyId, "routeNumber", new ArrayList<IFlag>());
     }
 }
