0c8373b77596b44a918e54009657f15a69a4d0d1
Oscar Evertsson <oscar.evertsson@live.com>
2015-10-13 13:52:32 +0200

    Moved all functionality regarding updating which vehicle you're on to the Activity instead of the fragment.

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagVehicle.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagVehicle.java
@@ -1,12 +1,23 @@
 package com.alive_n_clickin.commutity.presentation.flagreport;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.alive_n_clickin.commutity.MyApplication;
 import com.alive_n_clickin.commutity.R;
+import com.alive_n_clickin.commutity.application.IManager;
+import com.alive_n_clickin.commutity.domain.IVehicle;
+import com.alive_n_clickin.commutity.event.CurrentBusChangeEvent;
+import com.alive_n_clickin.commutity.event.WifiStateChangeEvent;
+import com.alive_n_clickin.commutity.infrastructure.WifiBroadcastReceiver;
+import com.alive_n_clickin.commutity.infrastructure.WifiHelper;
+import com.alive_n_clickin.commutity.util.event.IEvent;
+import com.alive_n_clickin.commutity.util.event.IObserver;
 
 /**
  * The main activity for the flag setting tool. The activity doesn't have any visual elements itself,
@@ -14,12 +25,37 @@ import com.alive_n_clickin.commutity.R;
  * that switching fragments is pain free.
  * @since 0.1
  */
-public class FlagVehicle extends FragmentActivity {
+public class FlagVehicle extends FragmentActivity implements IObserver {
+    private final static String ARG_POSITION = "position";
+    private int mCurrentPosition = -1;
+
+    private ActionBar actionBar;
+    private WifiHelper wifiHelper;
+
+    private IManager busManager;
+    private WifiBroadcastReceiver wifiBroadcastReceiver;
+
+    private MenuItem wifiDisabledIcon;
+    private MenuItem refreshIcon;
+
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flag_vehicle);
+        // Register observers
+        MyApplication application = (MyApplication) this.getApplicationContext();
+        this.busManager = application.getManager();
+        this.busManager.addObserver(this);
+        this.wifiBroadcastReceiver = application.getWifiBroadcastReceiver();
+        this.wifiBroadcastReceiver.addObserver(this);
+
+
+        this.actionBar = this.getActionBar();
+        this.wifiHelper = new WifiHelper(this);
+        this.actionBar.setTitle(R.string.loading_looking_for_vehicle);
+        updateWifiState();
+
         if (findViewById(R.id.content_frame) != null) {
             if (savedInstanceState != null) {
                 return;
@@ -28,13 +64,67 @@ public class FlagVehicle extends FragmentActivity {
         }
     }
 
+    private void updateBusText() {
+        if (this.busManager.isOnBus()) {
+            IVehicle bus = this.busManager.getCurrentBus();
+            String newText = getCurrentBusAsString(bus);
+            actionBar.setTitle(newText);
+            displayWifiEnabledIcon(true);
+        } else if (!wifiHelper.isWifiEnabled()) {
+            actionBar.setTitle(R.string.you_must_activate_wifi);
+            displayWifiEnabledIcon(false);
+        } else {
+            actionBar.setTitle(R.string.no_buses_near);
+            displayWifiEnabledIcon(true);
+        }
+    }
+
+
+    private String getCurrentBusAsString(IVehicle bus) {
+        StringBuilder newText = new StringBuilder();
+        newText.append(bus.getShortRouteName());
+        newText.append(" ");
+        newText.append(bus.getDestination());
+        return newText.toString();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_flag_vehicle, menu);
+        this.wifiDisabledIcon = menu.findItem(R.id.wifi_disabled_icon);
+        this.refreshIcon = menu.findItem(R.id.refresh_icon);
+        refreshIcon.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d("Testing","omaisneqwioneq");
+                return true;
+            }
+        });
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void updateWifiState() {
+        if (wifiHelper.isWifiEnabled()) {
+            wifiHelper.initiateWifiScan();
+            if(wifiDisabledIcon != null && refreshIcon != null) {
+                displayWifiEnabledIcon(true);
+            }
+        } else {
+            actionBar.setTitle(R.string.you_must_activate_wifi);
+        }
+    }
+
+    private void displayWifiEnabledIcon(boolean value) {
+        if (value) {
+            wifiDisabledIcon.setVisible(false);
+            refreshIcon.setVisible(true);
+        } else {
+            wifiDisabledIcon.setVisible(true);
+            refreshIcon.setVisible(false);
+        }
     }
 
     /**
@@ -54,5 +144,23 @@ public class FlagVehicle extends FragmentActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onEvent(IEvent event) {
+        if (event instanceof CurrentBusChangeEvent) {
+            handleCurrentBusChangeEvent((CurrentBusChangeEvent) event);
+        } else if (event instanceof WifiStateChangeEvent) {
+            handleWifiStateChangeEvent((WifiStateChangeEvent) event);
+        }
+    }
+
+
+    private void handleCurrentBusChangeEvent(CurrentBusChangeEvent event) {
+        this.updateBusText();
+    }
+
+    private void handleWifiStateChangeEvent(WifiStateChangeEvent event) {
+        this.updateBusText();
     }
 }
--- app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagVehicleFragment.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagVehicleFragment.java
@@ -1,27 +1,16 @@
 package com.alive_n_clickin.commutity.presentation.flagreport;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
-import com.alive_n_clickin.commutity.MyApplication;
 import com.alive_n_clickin.commutity.R;
-import com.alive_n_clickin.commutity.application.IManager;
 import com.alive_n_clickin.commutity.domain.Flag;
-import com.alive_n_clickin.commutity.domain.IVehicle;
-import com.alive_n_clickin.commutity.event.CurrentBusChangeEvent;
-import com.alive_n_clickin.commutity.event.WifiStateChangeEvent;
-import com.alive_n_clickin.commutity.infrastructure.WifiBroadcastReceiver;
-import com.alive_n_clickin.commutity.infrastructure.WifiHelper;
-import com.alive_n_clickin.commutity.util.event.IEvent;
-import com.alive_n_clickin.commutity.util.event.IObserver;
 
 import java.util.ArrayList;
 
@@ -30,17 +19,10 @@ import java.util.ArrayList;
  * view when the user clicks on a flag
  * @since 0.1
  */
-public class FlagVehicleFragment extends Fragment implements IObserver {
+public class FlagVehicleFragment extends Fragment {
     private final static String ARG_POSITION = "position";
     private int mCurrentPosition = -1;
 
-    private ActionBar actionBar;
-    private WifiHelper wifiHelper;
-    private MenuItem wifiDisabledIcon;
-    private MenuItem refreshIcon;
-
-    private IManager busManager;
-    private WifiBroadcastReceiver wifiBroadcastReceiver;
 
     private FlagViewAdapter flagAdapter;
     private ArrayList<FlagButton> flagButtons;
@@ -48,35 +30,7 @@ public class FlagVehicleFragment extends Fragment implements IObserver {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-        actionBar = getActivity().getActionBar();
-        wifiHelper = new WifiHelper(getActivity().getApplication().getApplicationContext());
-        this.wifiDisabledIcon = (MenuItem) getActivity().findViewById(R.id.wifi_disabled_icon);
-        this.refreshIcon = (MenuItem) getActivity().findViewById(R.id.refresh_icon);
-        updateWifiState();
         addTestButtons();
-    }
-
-    public
-
-    private void updateWifiState() {
-        if (wifiHelper.isWifiEnabled()) {
-            wifiHelper.initiateWifiScan();
-            setRefreshIconEnabled(true);
-        } else {
-            setRefreshIconEnabled(false);
-            actionBar.setTitle(R.string.you_must_activate_wifi);
-        }
-    }
-
-    private void setRefreshIconEnabled(boolean value) {
-        if (value) {
-            wifiDisabledIcon.setVisible(false);
-            refreshIcon.setVisible(true);
-        } else {
-            wifiDisabledIcon.setVisible(true);
-            refreshIcon.setVisible(false);
-        }
     }
 
     @Override
@@ -87,18 +41,8 @@ public class FlagVehicleFragment extends Fragment implements IObserver {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        // Register observers
-        MyApplication application = (MyApplication) this.getActivity().getApplicationContext();
-        this.busManager = application.getManager();
-        this.busManager.addObserver(this);
-        this.wifiBroadcastReceiver = application.getWifiBroadcastReceiver();
-        this.wifiBroadcastReceiver.addObserver(this);
-
         final View rootView = inflater.inflate(R.layout.fragment_flag_vehicle, container, false);
         flagAdapter = new FlagViewAdapter(getActivity(), flagButtons);
-
-        this.actionBar.setTitle(R.string.loading_looking_for_vehicle);
 
 
 
@@ -131,40 +75,14 @@ public class FlagVehicleFragment extends Fragment implements IObserver {
                 transaction.commit();
             }
         });
+
         return rootView;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-
-        this.busManager.removeObserver(this);
-        this.wifiBroadcastReceiver.removeObserver(this);
     }
-
-    private void updateBusText() {
-        if (this.busManager.isOnBus()) {
-            IVehicle bus = this.busManager.getCurrentBus();
-            String newText = getCurrentBusAsString(bus);
-            actionBar.setTitle(newText);
-            setRefreshIconEnabled(true);
-        } else if (!wifiHelper.isWifiEnabled()) {
-            actionBar.setTitle(R.string.you_must_activate_wifi);
-            setRefreshIconEnabled(false);
-        } else {
-            actionBar.setTitle(R.string.no_buses_near);
-            setRefreshIconEnabled(true);
-        }
-    }
-
-    private String getCurrentBusAsString(IVehicle bus) {
-        StringBuilder newText = new StringBuilder();
-        newText.append(bus.getShortRouteName());
-        newText.append(" ");
-        newText.append(bus.getDestination());
-        return newText.toString();
-    }
-
 
     /**
      * Adds the different buttons. At the moment it's all hard coded for testing purposes
@@ -182,20 +100,5 @@ public class FlagVehicleFragment extends Fragment implements IObserver {
         flagButtons.add(new FlagButton(R.drawable.flag_other_black_300px, getString(R.string.flag_other), Flag.Type.OTHER));
     }
 
-    @Override
-    public void onEvent(IEvent event) {
-        if (event instanceof CurrentBusChangeEvent) {
-            handleCurrentBusChangeEvent((CurrentBusChangeEvent) event);
-        } else if (event instanceof WifiStateChangeEvent) {
-            handleWifiStateChangeEvent((WifiStateChangeEvent) event);
-        }
-    }
 
-    private void handleCurrentBusChangeEvent(CurrentBusChangeEvent event) {
-        this.updateBusText();
-    }
-
-    private void handleWifiStateChangeEvent(WifiStateChangeEvent event) {
-        this.updateBusText();
-    }
 }
