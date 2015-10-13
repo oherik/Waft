531e27cb1c0a64e3a76844c1b33c948d926c8444
Erik Öhrn <erik.ohrn@outlook.com>
2015-10-13 09:36:52 +0200

    The app now implements the new flags

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagVehicleFragment.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagVehicleFragment.java
@@ -163,9 +163,9 @@ public class FlagVehicleFragment extends Fragment implements IObserver {
         flagButtons.add(new FlagButton(R.drawable.flag_full_300px, getString(R.string.flag_overcrowded), Flag.Type.OVERCROWDED));
         flagButtons.add(new FlagButton(R.drawable.flag_rowdy_300px, getString(R.string.flag_disturbance), Flag.Type.DISTURBANCES));
         flagButtons.add(new FlagButton(R.drawable.flag_delayed_300px, getString(R.string.flag_delayed), Flag.Type.DELAYED));
-        flagButtons.add(new FlagButton(R.drawable.flag_dirty_alt2_300px, getString(R.string.flag_messy), Flag.Type.MESSY));
+        flagButtons.add(new FlagButton(R.drawable.flag_dirty_300px, getString(R.string.flag_messy), Flag.Type.MESSY));
         flagButtons.add(new FlagButton(R.drawable.flag_pram_300px, getString(R.string.flag_pram), Flag.Type.NO_PRAMS));
-        flagButtons.add(new FlagButton(R.drawable.flag_other_black_300px, getString(R.string.flag_other), Flag.Type.OTHER));
+        flagButtons.add(new FlagButton(R.drawable.flag_other_300px, getString(R.string.flag_other), Flag.Type.OTHER));
     }
 
     @Override
