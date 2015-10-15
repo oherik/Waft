dfa48ed0e6ba7fd56064d3be3c668dc1e47a9226
Erik Öhrn <erik.ohrn@outlook.com>
2015-10-15 09:08:01 +0200

    Added comments and error checks

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/SlidingLayout.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/SlidingLayout.java
@@ -1,10 +1,16 @@
 package com.alive_n_clickin.commutity.presentation.flagreport;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
 
-        import android.content.Context;
-        import android.util.AttributeSet;
-        import android.widget.RelativeLayout;
-
+/**
+ * Used for making it possible for x- and y-fractions to be used when animating the
+ * detailed flag view fragment. This will make it possible, in the future, to have the
+ * fragment animate for a set percentage of the screen, something that wouldn't have
+ * been possible if just using the built in animations.
+ */
 public class SlidingLayout extends RelativeLayout {
 
     public SlidingLayout(Context context) {
@@ -19,15 +25,41 @@ public class SlidingLayout extends RelativeLayout {
         super(context, attrs, defStyle);
     }
 
-    public void setYFraction(final float fraction) {
+    /**
+     * Sets the translation of the layout, depending on a given fraction. It does this
+     * by measuring the height of the view, making it device-independent.
+     * @param fraction  The fraction of the screen the layout should extend to. Must be
+     *                  between 0 and 1.0
+     * @throws IndexOutOfBoundsException if the input is not between 0 and 1.0
+     */
+    public void setYFraction(float fraction) {
+        if(fraction<0 || fraction >1.0){
+            throw new IndexOutOfBoundsException("The fraction must be [0, 1.0]");
+        }
+        /*
+        Fixes a bug where the view would show for a frame, unanimated, on top of everything
+        else. This was because the height had not yet been measured. This if/else renders
+        the view invisible if no height has been set.
+         */
+        if(getHeight() == 0) {
+            setVisibility(View.INVISIBLE);
+        } else if(getVisibility()==View.INVISIBLE) {
+            setVisibility(View.VISIBLE);
+        }
         float translationY = getHeight() * fraction;
         setTranslationY(translationY);
     }
 
+    /**
+     * Measures the y translation in regards to the view height, and returns the fraction.
+     * @return  The fraction of the view height currently translated
+     */
     public float getYFraction() {
-        if (getHeight() == 0) {
+        //If the height hasn't yet been measured, don't bother with trying to divide by 0
+        float height = getHeight();
+        if (height == 0) {
             return 0;
         }
-        return getTranslationY() / getHeight();
+        return getTranslationY() / height;
     }
 }
