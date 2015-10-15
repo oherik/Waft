aca125bd62adb54c936a6a13b20a04d7d2176258
Oscar Evertsson <oscar.evertsson@live.com>
2015-10-15 13:46:51 +0200

    Added onClickListeners for each card and updated documentation.

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/RecyclerAdapter.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/RecyclerAdapter.java
@@ -1,6 +1,9 @@
 package com.alive_n_clickin.commutity.presentation.flagreport;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +16,7 @@ import com.alive_n_clickin.commutity.R;
 import java.util.List;
 
 /**
- * Created by OscarEvertsson on 14/10/15.
+ * This class handles creation of each Flag Type button and puts them in the RecyclerView element.
  */
 public class RecyclerAdapter extends  RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
     private List<FlagButton> flagButtonList;
@@ -22,7 +25,11 @@ public class RecyclerAdapter extends  RecyclerView.Adapter<RecyclerAdapter.ViewH
         this.flagButtonList = flagButtonList;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    /**
+     * This inner-class handles binding the elements together within the RecyclerAdapter.
+     * It sets the information for each FlagType card.
+     */
+    protected static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView flagInfo;
         public ImageView flagImg;
         public ViewHolder(View v) {
@@ -33,15 +40,39 @@ public class RecyclerAdapter extends  RecyclerView.Adapter<RecyclerAdapter.ViewH
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.flag_type_card,parent,false);
+    public ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
+        final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.flag_type_card,parent,false);
+        final ViewHolder holder = new ViewHolder(view);
+
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int currentItemPosition = holder.getLayoutPosition();
+                FlagButton currentButton = flagButtonList.get(currentItemPosition);
 
+                //Prepare arguments
+                FlagVehicleDetailFragment detailFragment = new FlagVehicleDetailFragment();
+                Bundle args = new Bundle();
+                args.putInt(FlagVehicleDetailFragment.getARG_POSITION(), currentItemPosition);
+
+                //Add flag data
+                args.putInt("flag_image_ID", currentButton.getImageID());
+                args.putString("flag_description", currentButton.getDescription());
+                args.putInt("flag_type_ID", currentButton.getType().getId());
+                detailFragment.setArguments(args);
+
+                //Make the switch
+                FragmentActivity fragmentActivity = (FragmentActivity) parent.getContext();
+                FragmentTransaction fragmentTransaction = fragmentActivity
+                        .getSupportFragmentManager()
+                        .beginTransaction();
+                fragmentTransaction.replace(R.id.content_frame, detailFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                fragmentTransaction.commit();
             }
         });
-        return new ViewHolder(view);
+        return holder;
     }
 
     @Override
