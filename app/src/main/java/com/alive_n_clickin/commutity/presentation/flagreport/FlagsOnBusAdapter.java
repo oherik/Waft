135fd08e43a82ff1f02625197b47d7fd517a4ad0
Oscar Evertsson <oscar.evertsson@live.com>
2015-10-17 19:11:02 +0200

    Updated interface to support getFlagsForBus

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagsOnBusAdapter.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/FlagsOnBusAdapter.java
@@ -3,19 +3,21 @@ package com.alive_n_clickin.commutity.presentation.flagreport;
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
-import com.alive_n_clickin.commutity.domain.IArrivingVehicle;
+import com.alive_n_clickin.commutity.domain.IFlag;
 
 import java.util.List;
 
 /**
  * Created by OscarEvertsson on 17/10/15.
  */
-public class RemoveFlagAdapter extends ArrayAdapter<IArrivingVehicle>{
-    private List<IArrivingVehicle> vehicleList;
+public class FlagsOnBusAdapter extends ArrayAdapter<IFlag>{
+    private List<IFlag> vehicleList;
 
-    public RemoveFlagAdapter(Context currentContext, List<IArrivingVehicle> arrivingVehicleList) {
+    public FlagsOnBusAdapter(Context currentContext, List<IFlag> arrivingVehicleList) {
         super(currentContext,0, arrivingVehicleList); //The second parameter is the resource ID for a layout file containing a layout to use when instantiating views. Making it 0 means we are not sending any resource file to the super class.
     }
+
+
 
 
 
