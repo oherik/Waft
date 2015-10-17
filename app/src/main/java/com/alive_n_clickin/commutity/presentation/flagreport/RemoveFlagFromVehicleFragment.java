46c6c359a0becf7464b969c07c1343df01683699
Oscar Evertsson <oscar.evertsson@live.com>
2015-10-17 19:26:53 +0200

    Created an updateFlags method.

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/RemoveFlagFromVehicleFragment.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/RemoveFlagFromVehicleFragment.java
@@ -1,17 +1,17 @@
 package com.alive_n_clickin.commutity.presentation.flagreport;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.alive_n_clickin.commutity.R;
-import com.alive_n_clickin.commutity.domain.IArrivingVehicle;
+import com.alive_n_clickin.commutity.domain.IFlag;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,31 +20,50 @@ import java.util.List;
  * This class represent the view where you can remove flags on the current vehicle you're on.
  */
 public class RemoveFlagFromVehicleFragment extends Fragment {
-    private List<IArrivingVehicle> vehicleList;
+    private List<IFlag> flagList;
 
-    private RemoveFlagAdapter removeFlagAdapter;
+    private FlagsOnBusAdapter flagsOnBusAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (flagList == null) {
+            flagList = new ArrayList<>();
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.remove_flag_fragment, container, false);
-        ListView removeFlagListView = (ListView) rootView.findViewById(R.id.remove_flag_listView);
+
         FloatingActionButton showPostFlagViewButton = (FloatingActionButton) rootView.
                 findViewById(R.id.showPostFlagViewButton);
-
         showPostFlagViewButton.setOnClickListener(new ShowPostFlagViewButtonListener());
 
-        this.removeFlagAdapter = new RemoveFlagAdapter(getContext(), vehicleList);
-        this.vehicleList = new ArrayList<>();
-       // removeFlagListView.setAdapter(this.removeFlagAdapter);
-
+        this.flagsOnBusAdapter = new FlagsOnBusAdapter(getContext(), flagList);
+        ListView flagListView = (ListView) rootView.findViewById(R.id.remove_flag_listView);
+        flagListView.setAdapter(this.flagsOnBusAdapter);
 
         return rootView;
     }
+
+    /**
+     * This method handles updating the current adapter with a new list of flags.
+     * @param flagList the new list to be displayed.
+     */
+    protected void updateFlags(List<IFlag> flagList) {
+        if(this.flagList == null) {
+            this.flagList = new ArrayList<>();
+        }
+        this.flagList.clear();
+        this.flagList.addAll(flagList);
+
+        if(flagsOnBusAdapter != null) {
+            flagsOnBusAdapter.clear();
+            flagsOnBusAdapter.addAll(flagList);
+        }
+    }
+
 
     /**
      * This class purposes is to handle onClick events from the showPostFlagViewButton
