039a7c7be027adfcd294870fd5a72b5290d21f76
Oscar Evertsson <oscar.evertsson@live.com>
2015-10-17 21:01:08 +0200

    Made the class a listener to bus events.

--- app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/RemoveFlagFromVehicleFragment.java
+++ app/src/main/java/com/alive_n_clickin/commutity/presentation/flagreport/RemoveFlagFromVehicleFragment.java
@@ -10,8 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.alive_n_clickin.commutity.MyApplication;
 import com.alive_n_clickin.commutity.R;
+import com.alive_n_clickin.commutity.application.IManager;
+import com.alive_n_clickin.commutity.domain.IElectriCityBus;
 import com.alive_n_clickin.commutity.domain.IFlag;
+import com.alive_n_clickin.commutity.util.event.CurrentBusChangeEvent;
+import com.alive_n_clickin.commutity.util.event.IEvent;
+import com.alive_n_clickin.commutity.util.event.IObserver;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,14 +25,21 @@ import java.util.List;
 /**
  * This class represent the view where you can remove flags on the current vehicle you're on.
  */
-public class RemoveFlagFromVehicleFragment extends Fragment {
+public class RemoveFlagFromVehicleFragment extends Fragment implements IObserver{
     private List<IFlag> flagList;
+    private IManager manager;
 
     private FlagsOnBusAdapter flagsOnBusAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Register observers
+        MyApplication application = (MyApplication) getActivity().getApplicationContext();
+        this.manager = application.getManager();
+        this.manager.addObserver(this);
+
         if (flagList == null) {
             flagList = new ArrayList<>();
         }
@@ -61,6 +74,22 @@ public class RemoveFlagFromVehicleFragment extends Fragment {
         if(flagsOnBusAdapter != null) {
             flagsOnBusAdapter.clear();
             flagsOnBusAdapter.addAll(flagList);
+        }
+    }
+
+    @Override
+    public void onEvent(IEvent event) {
+        if (event instanceof CurrentBusChangeEvent) {
+            handleCurrentBusChangeEvent((CurrentBusChangeEvent) event);
+        }
+    }
+
+    private void handleCurrentBusChangeEvent(CurrentBusChangeEvent event) {
+        IElectriCityBus bus = event.getBus();
+        if (bus != null) {
+            updateFlags(manager.getCurrentBus().getFlags());
+        } else {
+            updateFlags(new ArrayList<IFlag>());
         }
     }
 
