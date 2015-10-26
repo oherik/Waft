0a42546b83a4de75ef8fefc69032e427be035f0e
Mats Högberg <mats@hgbrg.se>
2015-10-26 11:45:55 +0100

    Updated IVasttrafikAdapter to return domain objects instead of response objects

--- app/src/main/java/com/alive_n_clickin/commutity/application/api/IVasttrafikAdapter.java
+++ app/src/main/java/com/alive_n_clickin/commutity/application/api/IVasttrafikAdapter.java
@@ -1,39 +1,41 @@
 package com.alive_n_clickin.commutity.application.api;
 
+import com.alive_n_clickin.commutity.domain.IArrivingVehicle;
 import com.alive_n_clickin.commutity.domain.IStop;
-import com.alive_n_clickin.commutity.infrastructure.api.response.JsonArrival;
-import com.alive_n_clickin.commutity.infrastructure.api.response.JsonStop;
 
 import java.util.List;
 
 /**
- * This interface represent the possible request to the Vasttrafik API combined with their responses.
+ * An interface for adapters between the Waft API and our domain.
  *
  * @since 0.1
  */
 public interface IVasttrafikAdapter {
-
      /**
       * This function gives you a list of the nearest stops to the location provided.
+      *
       * @param longitude
       * @param latitude
-      * @return a list of stops if the search was successful or null if failed.
+      * @return a list of stops if the search was successful. If the search was unsuccessful, an
+      * empty list is returned.
       */
-     List<JsonStop> getNearbyStations(double longitude,double latitude);
+     List<IStop> getNearbyStations(double longitude, double latitude);
 
      /**
       * This functions gives you a list of stops related to the search string you provide.
+      *
       * @param searchString
-      * @returns a list of stops if the search was successful.
-      * Otherwise returns null since there was no result.
+      * @returns a list of stops if the search was successful. If the search was unsuccessful, an
+      * empty list is returned.
       */
-     List<JsonStop> getSearchStops(String searchString);
+     List<IStop> getSearchStops(String searchString);
 
     /**
      * This function gives you a list of the 20 next vehicles headed to a certain stop at the current time
+     *
      * @param stop
-     * @return a list of vehicles headed to the specified stop, it the search was successful.
-     * Returns null if the search was unsuccessful.
+     * @return a list of vehicles headed to the specified stop, if the search was successful. If the
+     * search was unsuccessful, an empty list is returned.
      */
-     List<JsonArrival> getVehiclesHeadedToStop(IStop stop);
+     List<IArrivingVehicle> getVehiclesHeadedToStop(IStop stop);
 }
