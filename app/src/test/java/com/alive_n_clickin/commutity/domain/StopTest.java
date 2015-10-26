ba61428d6ad3d2ebc04c98e011fc68dd88b3dc87
Sebastian Sandberg <seb.sandberg@gmail.com>
2015-10-26 12:29:46 +0100

    tests the Stop class

---
+++ app/src/test/java/com/alive_n_clickin/commutity/domain/StopTest.java
@@ -0,0 +1,54 @@
+package com.alive_n_clickin.commutity.domain;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Sebastian on 2015-10-26.
+ */
+public class StopTest {
+
+    private long id = 10;
+    private String name = "MockStop";
+
+    @Test
+    public void testConstructorIllegalArguments() throws Exception {
+
+        boolean exception;
+        //test that creating a stop with no name returns exception
+        exception = false;
+        try{
+            new Stop(null, id);
+        }catch (NullPointerException e){
+            exception = true;
+        }
+        assertTrue(exception);
+
+        exception = false;
+        //test that including stop name will not result in exception
+        try{
+            new Stop(name, id);
+        } catch (NullPointerException e){
+            exception = true;
+        }
+        assertFalse(exception);
+
+
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+        Stop stop = new Stop(name,id);
+        assertEquals(name, stop.getName());
+    }
+
+    @Test
+    public void testGetId() throws Exception{
+        Stop stop = new Stop(name, id);
+        assertEquals(id, stop.getId());
+    }
+}
