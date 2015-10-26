14122e0f1d1760808b5d438d8ed58d6f3d087b3a
Mats Högberg <mats@hgbrg.se>
2015-10-26 15:54:33 +0100

    Added equals, toString, hashcode, constructor and null checks to Response

--- app/src/main/java/com/alive_n_clickin/commutity/infrastructure/api/response/Response.java
+++ app/src/main/java/com/alive_n_clickin/commutity/infrastructure/api/response/Response.java
@@ -1,11 +1,18 @@
 package com.alive_n_clickin.commutity.infrastructure.api.response;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
 
 /**
  * A class for responses to HTTP requests. Responses have a status and a body.
  */
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Response {
     @Getter private int status;
-    @Getter private String body;
+    @Getter @NonNull private String body;
 }
