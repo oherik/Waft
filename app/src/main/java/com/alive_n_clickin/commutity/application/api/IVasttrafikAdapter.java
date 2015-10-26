8c4e6be1c6ce74e05e265dd557212e9ebff95c60
Mats Högberg <mats@hgbrg.se>
2015-10-26 12:55:52 +0100

    Changed method names in IVasttrafikAdapter

--- app/src/main/java/com/alive_n_clickin/commutity/application/api/IVasttrafikAdapter.java
+++ app/src/main/java/com/alive_n_clickin/commutity/application/api/IVasttrafikAdapter.java
@@ -19,7 +19,7 @@ public interface IVasttrafikAdapter {
       * @return a list of stops if the search was successful. If the search was unsuccessful, an
       * empty list is returned.
       */
-     List<IStop> getNearbyStations(double longitude, double latitude);
+     List<IStop> getNearbyStops(double longitude, double latitude);
 
      /**
       * This functions gives you a list of stops related to the search string you provide.
@@ -28,7 +28,7 @@ public interface IVasttrafikAdapter {
       * @returns a list of stops if the search was successful. If the search was unsuccessful, an
       * empty list is returned.
       */
-     List<IStop> getSearchStops(String searchString);
+     List<IStop> searchForStops(String searchString);
 
     /**
      * This function gives you a list of the 20 next vehicles headed to a certain stop at the current time
@@ -37,5 +37,5 @@ public interface IVasttrafikAdapter {
      * @return a list of vehicles headed to the specified stop, if the search was successful. If the
      * search was unsuccessful, an empty list is returned.
      */
-     List<IArrivingVehicle> getVehiclesHeadedToStop(IStop stop);
+     List<IArrivingVehicle> getArrivalsForStop(IStop stop);
 }
