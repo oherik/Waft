e9ebf6806e300f9a0a39217ea893cc3013411b25
Mats Högberg <mats@hgbrg.se>
2015-10-28 00:49:20 +0100

    Moved responsibility from WaftApiConnection to WaftApi and ApiConnection

--- app/src/main/java/com/alive_n_clickin/commutity/infrastructure/api/response/Response.java
+++ app/src/main/java/com/alive_n_clickin/commutity/infrastructure/api/response/Response.java
@@ -15,4 +15,19 @@ import lombok.ToString;
 public class Response {
     @Getter private int status;
     @Getter @NonNull private String body;
+
+    /**
+     * Interprets a HTTP status code.
+     *
+     * @return true if the request that generated this response was ok (status is 200), otherwise false.
+     */
+    public boolean wasRequestSuccessful() {
+        switch (this.status) {
+            case 400:
+                return false;
+            case 200:
+                return true;
+        }
+        return false;
+    }
 }
