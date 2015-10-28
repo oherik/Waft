8565fe10ce664730131811f61b582eb254043f95
hjorthjort <rikard.hjort@gmail.com>
2015-10-28 17:32:04 +0100

    Move Json to vehicle conversion to api adapter

--- app/src/main/java/com/alive_n_clickin/commutity/application/VehicleFactory.java
+++ app/src/main/java/com/alive_n_clickin/commutity/application/VehicleFactory.java
@@ -3,19 +3,12 @@ package com.alive_n_clickin.commutity.application;
 import com.alive_n_clickin.commutity.application.api.ApiAdapterFactory;
 import com.alive_n_clickin.commutity.application.api.IElectriCityAdapter;
 import com.alive_n_clickin.commutity.application.api.IWaftAdapter;
-import com.alive_n_clickin.commutity.domain.ArrivingVehicle;
 import com.alive_n_clickin.commutity.domain.ElectriCityBus;
-import com.alive_n_clickin.commutity.domain.IArrivingVehicle;
 import com.alive_n_clickin.commutity.domain.IElectriCityBus;
 import com.alive_n_clickin.commutity.domain.IFlag;
 import com.alive_n_clickin.commutity.domain.IJourney;
-import com.alive_n_clickin.commutity.infrastructure.api.response.JsonArrival;
 
-import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
-
-import lombok.NonNull;
 
 /**
  * This is a factory for creating ready-to-use vehicles. The idea is that you ask this factory
@@ -26,7 +19,6 @@ import lombok.NonNull;
  * @since 0.2
  */
 public class VehicleFactory {
-    private static final String ELECTRICITY_SHORT_ROUTE_NAME = "55";
 
     private static final IWaftAdapter waftAdapter = ApiAdapterFactory.createWaftAdapter();
     private static final IElectriCityAdapter electriCityAdapter = ApiAdapterFactory.createElectricityAdapter();
@@ -50,25 +42,5 @@ public class VehicleFactory {
         return new ElectriCityBus(destination, journeyId, dgw, flags);
     }
 
-    /**
-     * Creates a new arriving vehicle object based on an API response.
-     *
-     * @param jsonArrival The API response
-     * @return  A new arriving vehicle based on the response
-     * @throws NullPointerException if the parameter is null
-     */
-    public static IArrivingVehicle getArrivingVehicle(@NonNull JsonArrival jsonArrival) {
-        String direction = jsonArrival.getDirection();
-        String shortRouteName = jsonArrival.getSname();
-        String journeyId = jsonArrival.getJourneyid();
-        Date realArrival = jsonArrival.getRealArrival();
-        int lineColor = jsonArrival.getLineColor();
 
-        List<IFlag> flags = new LinkedList<>();
-        if (shortRouteName.equals(ELECTRICITY_SHORT_ROUTE_NAME)) {
-            flags = waftAdapter.getFlagsForVehicle(journeyId);
-        }
-
-        return new ArrivingVehicle(direction, shortRouteName, journeyId, realArrival, flags, lineColor);
-    }
 }
