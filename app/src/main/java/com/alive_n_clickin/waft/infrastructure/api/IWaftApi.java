29e55474838e8f32fc3f6ffa19d33728dc4bf810
hjorthjort <rikard.hjort@gmail.com>
2015-10-29 10:05:58 +0100

    rename packages

---
+++ app/src/main/java/com/alive_n_clickin/waft/infrastructure/api/IWaftApi.java
@@ -0,0 +1,40 @@
+package com.alive_n_clickin.waft.infrastructure.api;
+
+import com.alive_n_clickin.waft.infrastructure.api.response.JsonFlag;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * An interface for Waft's API.
+ */
+public interface IWaftApi {
+    /**
+     * Returns a list of all flags for a specific journey.
+     *
+     * @param journeyId the journey to get flags for.
+     * @return a list of all flags for a specific journey. If there are no flags for the journey,
+     * an empty list is returned.
+     */
+    List<JsonFlag> getFlagsForJourney(String journeyId);
+
+    /**
+     * Adds a flag to the Waft database with the specified info.
+     *
+     * @param dgw the DGW for the bus to flag.
+     * @param journeyId the journey id the bus is on currently.
+     * @param flagTypeId the id of the flag type.
+     * @param comment a comment for the flag.
+     * @param createdTime the time the flag was created.
+     * @return true if the bus was successfully flagged, otherwise false.
+     */
+    boolean addFlag(String dgw, String journeyId, int flagTypeId, String comment, Date createdTime);
+
+    /**
+     * Removes a flag from the Waft database.
+     *
+     * @param id the id of the flag to remove.
+     * @return true if the removal was successful, otherwise false.
+     */
+    boolean deleteFlag(String id);
+}
---
+++ app/src/main/java/com/alive_n_clickin/waft/infrastructure/api/response/JsonJourneyInfo.java
@@ -0,0 +1,15 @@
+package com.alive_n_clickin.waft.infrastructure.api.response;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+
+/**
+ * @since 0.2
+ */
+public class JsonJourneyInfo implements Serializable {
+    @Getter private String resourceSpec;
+    @Getter private long timestamp;
+    @Getter private String value;
+    @Getter private String gateway;
+}
