09d0c2c6cbc81a0d1ff671698155997a9757df16
Erik Öhrn <erik.ohrn@outlook.com>
2015-10-30 15:24:06 +0100

    Removed author tag

--- app/src/main/java/com/alive_n_clickin/waft/application/ConnectionTimedOutEvent.java
+++ app/src/main/java/com/alive_n_clickin/waft/application/ConnectionTimedOutEvent.java
@@ -2,9 +2,5 @@ package com.alive_n_clickin.waft.application;
 
 import com.alive_n_clickin.waft.util.event.IEvent;
 
-/**
- * @author hjorthjort
- *         Created 30/10/15
- */
 public class ConnectionTimedOutEvent implements IEvent {
 }
